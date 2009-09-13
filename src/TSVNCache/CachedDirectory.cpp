YueLinHo/TortoiseSVN

commit 8fd518a7a0c229a3c38d92e10cff7f549c022a38

Prevent a stack overflow under certain circumstances by preventing an endless recursion if the path doesn't change.
git-svn-id: https://svn.osdn.net/svnroot/tortoisesvn/trunk@16659 154be670-7969-47af-9ab0-c6fb378d2295

src/TSVNCache/CachedDirectory.cpp
@@ -463,7 +463,7 @@ CStatusCacheEntry CCachedDirectory::GetStatusForMember(const CTSVNPath& path, bo
 
 		// If the status *still* isn't valid here, it means that 
 		// the current directory is unversioned, and we shall need to ask its children for info about themselves
-		if (dirEntry)
+		if ((dirEntry)&&(!dirEntry->m_directoryPath.IsEquivalentToWithoutCase(path)))
 			return dirEntry->GetStatusForMember(path,bRecursive);
 		CSVNStatusCache::Instance().AddFolderForCrawling(path);
 		return CStatusCacheEntry();

commit 186975ff58bac3ff9f9151cfb60c4aa2fcaf6fa9

Merge r16725, r16979 and r17118 from trunk: * Patch fix a bug introduced in r16659 (Prevent a stack overflow under certain circumstances by preventing an endless recursion if the path doesn't change). This change effectively disabled 'return dirEntry->GetStatusForMember(path,bRecursive)', because (!dirEntry->m_directoryPath.IsEquivalentToWithoutCase(path)) can never be true. * Set the run flag to false as early as possible. * Use a different name for the cache on x64 systems.
git-svn-id: https://svn.osdn.net/svnroot/tortoisesvn/branches/1.6.x@17167 154be670-7969-47af-9ab0-c6fb378d2295

src/TSVNCache/CachedDirectory.cpp
@@ -489,7 +489,7 @@ CStatusCacheEntry CCachedDirectory::GetStatusForMember(const CTSVNPath& path, bo
 
 		// If the status *still* isn't valid here, it means that 
 		// the current directory is unversioned, and we shall need to ask its children for info about themselves
-		if ((dirEntry)&&(!dirEntry->m_directoryPath.IsEquivalentToWithoutCase(path)))
+		if ((dirEntry)&&(dirEntry != this))
 			return dirEntry->GetStatusForMember(path,bRecursive);
 		// add the path for crawling: if it's really unversioned, the crawler will
 		// only check for the admin dir and do nothing more. But if it is

src/TSVNCache/SVNStatusCache.cpp
@@ -26,6 +26,12 @@
 //////////////////////////////////////////////////////////////////////////
 #define CACHEDISKVERSION 2
 
+#ifdef WIN64
+#define STATUSCACHEFILENAME _T("\\cache64")
+#else
+#define STATUSCACHEFILENAME _T("\\cache")
+#endif
+
 CSVNStatusCache* CSVNStatusCache::m_pInstance;
 
 CSVNStatusCache& CSVNStatusCache::Instance()
@@ -55,7 +61,7 @@ void CSVNStatusCache::Create()
 			if (CreateDirectory(path, NULL)==0)
 				goto error;
 		}
-		_tcscat_s(path, MAX_PATH, _T("\\cache"));
+		_tcscat_s(path, MAX_PATH, STATUSCACHEFILENAME);
 		// in case the cache file is corrupt, we could crash while
 		// reading it! To prevent crashing every time once that happens,
 		// we make a copy of the cache file and use that copy to read from.
@@ -159,7 +165,7 @@ bool CSVNStatusCache::SaveCache()
 		_tcscat_s(path, MAX_PATH, _T("\\TSVNCache"));
 		if (!PathIsDirectory(path))
 			CreateDirectory(path, NULL);
-		_tcscat_s(path, MAX_PATH, _T("\\cache"));
+		_tcscat_s(path, MAX_PATH, STATUSCACHEFILENAME);
 		_tfopen_s(&pFile, path, _T("wb"));
 		if (pFile)
 		{

src/TSVNCache/TSVNCache.cpp
@@ -336,6 +336,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_QUERYENDSESSION:
 		{
 			ATLTRACE("WM_QUERYENDSESSION\n");
+			bRun = false;
 			if (CSVNStatusCache::Instance().WaitToWrite(200))
 			{
 				CSVNStatusCache::Instance().Stop();
@@ -350,12 +351,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_QUIT:
 		{
 			ATLTRACE("WM_CLOSE/DESTROY/ENDSESSION/QUIT\n");
+			bRun = false;
 			CSVNStatusCache::Instance().WaitToWrite();
 			CSVNStatusCache::Instance().Stop();
 			CSVNStatusCache::Instance().SaveCache();
 			if (message != WM_QUIT)
 				PostQuitMessage(0);
-			bRun = false;
 			return 1;
 		}
 		break;